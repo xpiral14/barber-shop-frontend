4938dbbd3d55a2c2176948de9e154931930aba28
xpiral14 <sa.reis1999@gmail.com>
2020-06-02 14:57:34 -0300

    changed private route logic

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -8,23 +8,37 @@ import {
 import { useAuth } from '../hooks/auth';
 import { UserType } from '../models/User';
 import baseUserRoute from '../config/defaultRoutes';
+import CostumerLayout from '../pages/_Layout/CostumerLayout';
+import BarberLayout from '../pages/_Layout/BarberLayout';
+import PublicLayout from '../pages/_Layout/PublicLayout';
 
 interface RouteProps extends ReactDOMRouteProps {
   component: React.ComponentType;
   authTypes?: UserType[];
 }
-
+const layouts = {
+  Costumer: CostumerLayout,
+  Barber: BarberLayout,
+  Public: PublicLayout,
+};
 const Route: React.FC<RouteProps> = ({
   authTypes,
   component: Component,
   ...rest
 }) => {
   const { user } = useAuth();
+
   return (
     <ReactDOMRoute
       {...rest}
       render={({ location }) => {
-        if (!authTypes?.length && !user) return <Component />;
+        if (!authTypes?.length && !user)
+          return (
+            <layouts.Public>
+              <Component />
+            </layouts.Public>
+          );
+
         if (!user && authTypes?.length)
           return (
             <Redirect
@@ -34,7 +48,7 @@ const Route: React.FC<RouteProps> = ({
               }}
             />
           );
-          // se a rota é publica e há um usuário logado
+        // se a rota é publica e há um usuário logado
         if (user && !authTypes?.length)
           return (
             <Redirect
@@ -44,8 +58,21 @@ const Route: React.FC<RouteProps> = ({
               }}
             />
           );
-        
-        return <Component />;
+        let PrivateLayout = layouts.Barber;
+        switch (user.userType.id) {
+          case 1:
+            PrivateLayout = layouts.Barber;
+            break;
+          case 2:
+            PrivateLayout = layouts.Costumer;
+            break;
+        }
+
+        return (
+          <PrivateLayout>
+            <Component />
+          </PrivateLayout>
+        );
       }}
     />
   );
