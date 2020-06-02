3a089a6273bcaadd5a9a67642a898199242a1e0d
xpiral14 <sa.reis1999@gmail.com>
2020-06-01 22:35:55 -0300

    added api  service

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,16 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+export const baseRoutePaths = {
+  barber: "/barber",
+  costumer: "/costumer",
+  admin: "/admin"
+}
+const {barber} = baseRoutePaths
+export const apiRoutes = {
+  barber:{
+    appointmentAt: (barberId:number) => `${barber}/${barberId}/appointment`,
+    shouAvailableTime: (barberId: number) => `${barber}/${barberId}/available-time`,
+  }
+}
 export default api;
