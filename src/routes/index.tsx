033cccc16dc2f8010bba0f46d42a31e3603db429
xpiral14 <sa.reis1999@gmail.com>
2020-06-06 02:35:10 -0300

    added create costumer route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,11 +8,12 @@ import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import Dashboard from '../pages/Barber/Dashboard';
+import Profile from '../pages/Barber/Profile';
 import { UserType } from '../models/User';
 import DashBoardCostumer from '../pages/Costumer/Dashboard';
 import MakeAppointment from '../pages/Costumer/MakeAppointment';
+import CreateCostumer from '../pages/Barber/CreateCostumer';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -24,6 +25,11 @@ const Routes: React.FC = () => (
     <Route
       path="/barber/dashboard"
       component={Dashboard}
+      authTypes={[UserType.BARBER]}
+    />
+    <Route
+      path="/barber/costumer/create"
+      component={CreateCostumer}
       authTypes={[UserType.BARBER]}
     />
     <Route path="/profile" component={Profile} authTypes={[UserType.BARBER]} />
